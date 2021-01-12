0519ea3781786af678e60a8a904d530592629167
jmg <jmg@nve.no>
2021-01-12 20:04:37 +0100

    run tests for all configurations

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,6 @@ using FSM
 using CSV
 using DataFrames
 
-
-data_ref = CSV.File("../data/out_CdP_0506.txt", header=["year", "month", "day", "hour", "alb", "Roff", "HS", "SWE", "Tsurf", "Tsoil"], delim=" ", ignorerepeated=true) |> DataFrame
-
 data_force = CSV.File("../data/met_CdP_0506.csv") |> DataFrame
 
 input = Input{Float32}(
@@ -21,22 +18,49 @@ input = Input{Float32}(
     data_force.RH,
     data_force.Ua,
     data_force.Ps,
-)
-
-
-ebm = EBM{Float32}()
-
-snowdepth = zeros(Float32, length(input.Ta))
-SWE = zeros(Float32, length(input.Ta))
-
-run!(ebm, snowdepth, SWE, input)
+    )
 
 @testset "complete sim" begin
 
-    err = SWE - data_ref.SWE
+    files_ref = readdir("data")
 
-    @test maximum(abs.(err)) < 10
+    for file_ref in files_ref
 
-    @info maximum(abs.(err))
+        # Initilize model
+
+        ebm = EBM{Float32}(
+            am=parse(Int, file_ref[14]),
+            cm=parse(Int, file_ref[15]),
+            dm=parse(Int, file_ref[16]),
+            em=parse(Int, file_ref[17]),
+            hm=parse(Int, file_ref[18]),
+            zT=1.5,
+            zvar=false,
+            Tsoil=[282.98, 284.17, 284.70, 284.70]
+        )
+
+        cn = Constants{Float32}()
+
+        snowdepth = similar(input.Ta)
+        SWE = similar(input.Ta)
+        Tsurf = similar(input.Ta)
+
+        # Run model
+
+        run!(ebm, cn, snowdepth, SWE, Tsurf, input)
+
+        # Read reference data
+
+        data_ref = CSV.File(joinpath("data", file_ref), header=["year", "month", "day", "hour", "alb", "Roff", "HS", "SWE", "Tsurf", "Tsoil"], delim=" ", ignorerepeated=true) |> DataFrame
+
+        # Compute error
+
+        err = SWE - data_ref.SWE
+
+        @test maximum(abs.(err)) < 10
+
+        @info maximum(abs.(err))
+
+    end
 
 end
