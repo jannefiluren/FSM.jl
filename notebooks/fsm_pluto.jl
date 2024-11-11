633b29778066ad134180132b615c3cb9e9d85b9a
jannefiluren <jan.magnusson@gmail.com>
2024-11-11 20:33:19 +0100

    Update pluto notebook

--- notebooks/fsm_pluto.jl
+++ notebooks/fsm_pluto.jl
@@ -1,32 +1,34 @@
 ### A Pluto.jl notebook ###
-# v0.19.4
+# v0.20.3
 
 using Markdown
 using InteractiveUtils
 
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
+    #! format: off
     quote
         local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
     end
+    #! format: on
 end
 
 # ╔═╡ fc72c7b0-d9c8-11ec-1e41-07be1f03aeb5
 begin
     import Pkg
     # activate the shared project environment
-    Pkg.activate()
+    Pkg.activate("/home/jannefiluren/.julia/dev/FSM")
     # instantiate, i.e. make sure that all packages are downloaded
-    Pkg.instantiate()
+    # Pkg.instantiate()
 
 	using FSM
 	using CSV
 	using DataFrames
 	using Plots
-	using PlutoUI
+	using PlutoUI	
 end
 
 # ╔═╡ 9bfc0a63-02a7-4e0a-8af9-5aaf13a86aa6
@@ -36,40 +38,32 @@ md"""
 
 # ╔═╡ 41499d7f-77f3-472d-8e89-dd0eeec60595
 md"""
-Snow albedo model: $(@bind am_tmp Scrubbable(0:1; default=0)) \
-Snow conductivity model: $(@bind cm_tmp Scrubbable(0:1; default=0)) \
-Snow density model: $(@bind dm_tmp Scrubbable(0:1; default=0)) \
-Surface exchange model: $(@bind em_tmp Scrubbable(0:1; default=0)) \
-Snow hydraulics model: $(@bind hm_tmp Scrubbable(0:1; default=0)) \
-Longwave offset: $(@bind LW_offset Scrubbable(-20:2:20; default=0)) \
-Short wave offset: $(@bind SW_offset Scrubbable(0.5:0.1:1.5; default=1)) \
-Air temperature offset: $(@bind Ta_offset Scrubbable(-3:0.5:3; default=0)) \
-Wind speed offset: $(@bind Ua_offset Scrubbable(0.2:0.1:2; default=1)) \
+Snow albedo model: $(@bind am_tmp Select([0,1])) \
+Snow conductivity model: $(@bind cm_tmp Select([0,1])) \
+Snow density model: $(@bind dm_tmp Select([0,1])) \
+Surface exchange model: $(@bind em_tmp Select([0,1])) \
+Snow hydraulics model: $(@bind hm_tmp Select([0,1])) \
+Longwave offset: $(@bind LW_offset NumberField(-20:2:20; default=0)) \
+Short wave offset: $(@bind SW_offset NumberField(0.5:0.1:1.5; default=1)) \
+Air temperature offset: $(@bind Ta_offset NumberField(-3:0.5:3; default=0)) \
+Wind speed offset: $(@bind Ua_offset NumberField(0.2:0.1:2; default=1.0))\
 """
 
 # ╔═╡ 4be3cfe4-810c-4f2b-84e6-57fa6f65329c
 md"""
-## Result plots...
+## Results...
 """
 
-# ╔═╡ d7b4501c-228b-4c6d-a862-d9b360aa34fa
-md"""
-## Main code...
-"""
-
-# ╔═╡ f646ce14-b23c-4a38-8426-5a8a7394bb73
+# ╔═╡ 5ebbb041-d1ad-495f-a1d8-821dbf9904d0
 begin
-	
+
+	# Get physics options
+
 	am = string(am_tmp)
 	cm = string(cm_tmp)
 	dm = string(dm_tmp)
 	em = string(em_tmp)
 	hm = string(hm_tmp)
-	
-end
-
-# ╔═╡ 5ebbb041-d1ad-495f-a1d8-821dbf9904d0
-begin
 
 	# Load forcing data
 
@@ -128,12 +122,10 @@ begin
 		Tsoil=[282.98, 284.17, 284.70, 284.70]
 	)
 
-	run!(ebm, cn, hs_base, swe_base, tsurf_base, input)
+	timing = @elapsed run!(ebm, cn, hs_base, swe_base, tsurf_base, input)
 
-end
+	# Load reference simulations from fortran code
 
-# ╔═╡ 61162269-cdf3-4db4-a52a-d9ac6ef94b81
-begin
 	file_ref = "out_CdP_0506_$(am)$(cm)$(dm)$(em)$(hm).txt"
 
 	data_ref = CSV.File(joinpath(fsm_path, "..", "test", "data", file_ref), header=["year", "month", "day", "hour", "alb", "Roff", "snowdepth", "SWE", "Tsurf", "Tsoil"], delim=" ", ignorerepeated=true) |> DataFrame
@@ -142,7 +134,12 @@ begin
 	swe_ref = data_ref.SWE
 	tsurf_ref = data_ref.Tsurf
 
+	nothing
+
 end
+
+# ╔═╡ 03a37641-b20a-4108-9250-bbbe212505c0
+md"""Model run time: $(timing) seconds"""
 
 # ╔═╡ 41c73e0c-d179-49a3-9d71-3ccaa664fb77
 begin
@@ -165,19 +162,13 @@ begin
 		plot!(tsurf_base, label="Base")
 end
 
-# ╔═╡ b8acf6ac-fd6f-449f-ba18-c7f91d196a3d
-file_ref
-
 # ╔═╡ Cell order:
-# ╟─fc72c7b0-d9c8-11ec-1e41-07be1f03aeb5
 # ╟─9bfc0a63-02a7-4e0a-8af9-5aaf13a86aa6
 # ╟─41499d7f-77f3-472d-8e89-dd0eeec60595
+# ╟─03a37641-b20a-4108-9250-bbbe212505c0
 # ╟─4be3cfe4-810c-4f2b-84e6-57fa6f65329c
 # ╟─41c73e0c-d179-49a3-9d71-3ccaa664fb77
 # ╟─0b1516f2-041a-4358-9e30-5d3dcb24db1e
 # ╟─ff129b48-7221-4803-8766-45acc210d512
-# ╟─d7b4501c-228b-4c6d-a862-d9b360aa34fa
-# ╠═f646ce14-b23c-4a38-8426-5a8a7394bb73
-# ╠═5ebbb041-d1ad-495f-a1d8-821dbf9904d0
-# ╠═61162269-cdf3-4db4-a52a-d9ac6ef94b81
-# ╟─b8acf6ac-fd6f-449f-ba18-c7f91d196a3d
+# ╟─5ebbb041-d1ad-495f-a1d8-821dbf9904d0
+# ╟─fc72c7b0-d9c8-11ec-1e41-07be1f03aeb5
