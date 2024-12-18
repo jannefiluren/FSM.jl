c6bdaf2f8249dd2af0ef190b5fab4f341132de09
jannefiluren <jan.magnusson@gmail.com>
2024-12-18 09:16:53 +0100

    Small cleaning of code

--- notebooks/surf_props.jl
+++ notebooks/surf_props.jl
@@ -1,20 +1,27 @@
 ### A Pluto.jl notebook ###
-# v0.12.17
+# v0.20.3
 
 using Markdown
 using InteractiveUtils
 
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
+    #! format: off
     quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
         local el = $(esc(element))
-        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
     end
+    #! format: on
 end
 
 # ╔═╡ fff0ff7a-4a94-11eb-1821-fbc6796d2e2d
 begin
+	import Pkg
+    Pkg.activate(joinpath(@__DIR__,".."))
+    Pkg.instantiate()
+	
 	using FSM
 	using PlutoUI
 	using Plots
@@ -25,21 +32,32 @@ md"""
 # Albedo models
 """
 
-# ╔═╡ 7595e612-4ab0-11eb-3a50-eb114ed9507d
+# ╔═╡ f866f932-4ab0-11eb-0c58-0dd947ff1341
 md"""
-## Model setup
+## Diagnostic albedo model
 """
 
-# ╔═╡ 24c749a8-4a95-11eb-1fc8-f34a2d08bdbe
-ebm = EBM{Float64}()
+# ╔═╡ 1427ffea-4ab1-11eb-1862-254bcabf97ac
+md"""
+## Prognostic albedo model
+	
+Add snow at hour = 100
+"""
 
-# ╔═╡ d66ab284-4a96-11eb-261d-b73fc2de22c2
-cn = Constants{Float64}()
+# ╔═╡ 5512e630-4ab3-11eb-2347-8f4afbd5f228
+@bind Sf_one Slider(0:200, show_value=true)
 
 # ╔═╡ fa0dd0ec-4ab2-11eb-34ce-d16073a73f39
 md"""
-## Albedo function
+## Actual computations
 """
+
+# ╔═╡ 24c749a8-4a95-11eb-1fc8-f34a2d08bdbe
+begin
+	ebm = EBM{Float64}()
+	cn = Constants{Float64}()
+	nothing
+end
 
 # ╔═╡ 4e6b1406-4a95-11eb-0827-432daf4b223e
 function compute_albedo(ebm::EBM, cn::Constants, Sf)
@@ -66,7 +84,7 @@ function compute_albedo(ebm::EBM, cn::Constants, Sf)
 
 end
 
-# ╔═╡ 0b59e04c-4aaf-11eb-33ff-f9e05476a28f
+# ╔═╡ be13ab46-4aaf-11eb-1f96-752bcbef74f6
 begin
 	Tsurf = cn.Tm-10:0.01:cn.Tm+10
 	Sf = 0
@@ -77,30 +95,11 @@ begin
 		compute_albedo(eb, cn, Sf)
 		push!(albedo, eb.albs)
 	end
+
+	plot(Tsurf, albedo, xlabel = "Temperature (K)", ylabel = "Albedo (-)", title = "Albedo model = 0")
 end
 
-# ╔═╡ f866f932-4ab0-11eb-0c58-0dd947ff1341
-md"""
-## Diagnostic albedo model
-"""
-
-# ╔═╡ be13ab46-4aaf-11eb-1f96-752bcbef74f6
-plot(Tsurf, albedo, xlabel = "Temperature (K)", ylabel = "Albedo (-)", title = "Albedo model = 0")
-
-# ╔═╡ 1427ffea-4ab1-11eb-1862-254bcabf97ac
-md"""
-## Prognostic albedo model
-"""
-
-# ╔═╡ 798de214-4ab3-11eb-0d8e-f7fe85ae089e
-md"""
-Add snow at hour = 100
-"""
-
-# ╔═╡ 5512e630-4ab3-11eb-2347-8f4afbd5f228
-@bind Sf_one Slider(0:200, show_value=true)
-
-# ╔═╡ 20190330-4ab1-11eb-33b4-e9ccd85a469a
+# ╔═╡ 2016de88-4ab2-11eb-3ca1-a75c6752bc71
 begin
 	eb_cold = EBM{Float64}(Tsurf = 271.15, am = 1)
 	eb_warm = EBM{Float64}(Tsurf = 273.15, am = 1)
@@ -122,10 +121,7 @@ begin
 		push!(albs_warm, eb_warm.albs)
 		push!(t_prog, t)
 	end
-end
-
-# ╔═╡ 2016de88-4ab2-11eb-3ca1-a75c6752bc71
-begin
+	
 	plot(t_prog, albs_cold)
 	plot!(t_prog, albs_warm)
 	plot!(xlabel = "Temperature (K)", ylabel = "Albedo (-)", title = "Albedo model = 1")
@@ -134,17 +130,12 @@ end
 
 # ╔═╡ Cell order:
 # ╟─7e7db788-4a96-11eb-3a41-2115cc7d3b58
-# ╟─7595e612-4ab0-11eb-3a50-eb114ed9507d
-# ╟─24c749a8-4a95-11eb-1fc8-f34a2d08bdbe
-# ╟─d66ab284-4a96-11eb-261d-b73fc2de22c2
-# ╟─fff0ff7a-4a94-11eb-1821-fbc6796d2e2d
-# ╟─fa0dd0ec-4ab2-11eb-34ce-d16073a73f39
-# ╠═4e6b1406-4a95-11eb-0827-432daf4b223e
-# ╟─0b59e04c-4aaf-11eb-33ff-f9e05476a28f
 # ╟─f866f932-4ab0-11eb-0c58-0dd947ff1341
 # ╟─be13ab46-4aaf-11eb-1f96-752bcbef74f6
 # ╟─1427ffea-4ab1-11eb-1862-254bcabf97ac
-# ╟─20190330-4ab1-11eb-33b4-e9ccd85a469a
-# ╟─798de214-4ab3-11eb-0d8e-f7fe85ae089e
 # ╟─5512e630-4ab3-11eb-2347-8f4afbd5f228
 # ╟─2016de88-4ab2-11eb-3ca1-a75c6752bc71
+# ╟─fa0dd0ec-4ab2-11eb-34ce-d16073a73f39
+# ╟─24c749a8-4a95-11eb-1fc8-f34a2d08bdbe
+# ╟─4e6b1406-4a95-11eb-0827-432daf4b223e
+# ╟─fff0ff7a-4a94-11eb-1821-fbc6796d2e2d
